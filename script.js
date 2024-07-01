78311a03e1368d51ca8b5794ef5828ae262acd0c
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-07-01 19:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 function convertToRoman(num) {
-  	const romanSymbols = [
-        ['M', 1000],
-        ['D', 500],
-        ['C', 100],
-        ['L', 50],
-        ['X', 10],
-        ['V', 5],
-        ['I', 1]
-    ];
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
-    let result = '';
+  //your code here
+	let result = "";
 
-    for (let i = 0; i < romanSymbols.length; i++) {
-        const [symbol, value] = romanSymbols[i];
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  for (let key in obj) {
+    const [symbol, value] = obj[key];
+    const count = Math.floor(num / value);
+
+    if (count >= 1) {
+      result += symbol.repeat(count);
+      num -= value * count;
     }
 
-    return result;
+    // Handle cases like 4, 40, 9, etc. (using subtraction notation)
+    if (key % 2 === 0 && obj[key + 2] && num >= obj[key + 2][1] - value)
+	{
+      result += obj[key + 2][0] + symbol;
+      num -= obj[key + 2][1] - value;
+    }
+  }
 
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 
 
